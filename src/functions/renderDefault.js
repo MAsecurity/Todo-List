9d72684eadd410ba7679ffa27f8a9ef45f7d8001
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-11-06 17:58:37 +0000

    Update renderDefault.js and include image

--- src/functions/renderDefault.js
+++ src/functions/renderDefault.js
@@ -1,4 +1,5 @@
 import "../style.css";
+import unDrawImg from "../images/undrawImg.svg";
 function getRenderDefault(){
   const contentElement = document.createElement("div");
   contentElement.setAttribute("id", "content");
@@ -8,10 +9,17 @@ function getRenderDefault(){
   sideBar.classList.add("sidebar");
   const mainContent = document.createElement("div");
   mainContent.classList.add("main");
+  const currentContent = document.createElement("div");
+  currentContent.classList.add("current-content");
+  const mainActualImg = document.createElement("img");
+  mainActualImg.style.cssText = "width:50%; height:50%"
+  mainActualImg.src = unDrawImg;
   document.body.appendChild(contentElement);
   contentElement.appendChild(navBar);
   contentElement.appendChild(sideBar);
   contentElement.appendChild(mainContent);
+  mainContent.appendChild(currentContent);
+  currentContent.appendChild(mainActualImg);
 
 };
 export {getRenderDefault};
