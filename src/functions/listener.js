7f521c6ab2fe8e3e0cee7cfada7075f7c689ada8
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-11-23 23:42:44 +0000

    Update files

--- src/functions/listener.js
+++ src/functions/listener.js
@@ -1,9 +1,11 @@
 import { dialog } from "./renderDialog";
+import { projects } from "./renderProjects";
 function listen(){
   const createTaskBtn = document.querySelector("#createTodo");
   createTaskBtn.addEventListener("click",() =>{
     dialog();
   });
   const sidebarProjectsIcon = document.querySelector(".sidebar-projects .material-symbols-outlined");
+  sidebarProjectsIcon.addEventListener("click", projects);
 }
 export {listen}
--- src/functions/renderProjects.js
+++ src/functions/renderProjects.js
@@ -1,4 +1,5 @@
-import { todoArray } from "./addTodo";
-function projects(todoArray){
-  
+function projects(){
+  const checkSideBarIcon = document.querySelector(".sidebar-projects .material-symbols-outlined");
+  checkSideBarIcon.textContent = (checkSideBarIcon.textContent == 'expand_circle_right') ? checkSideBarIcon.textContent = 'expand_circle_down': checkSideBarIcon.textContent= 'expand_circle_right'; 
 }
+export{projects}
