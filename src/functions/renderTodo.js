291f1a8c30e78521163536c69e9fffdc9e7b64c4
MAsecurity <108958617+MAsecurity@users.noreply.github.com>
2023-11-29 07:30:29 +0000

    Add title , priority and date

--- src/functions/renderTodo.js
+++ src/functions/renderTodo.js
@@ -9,7 +9,32 @@ function renderTodo(arrayOfObj){
     const todoCard = document.createElement("div");
     todoCard.classList.add("card");
     todoCard.setAttribute("index",`${index}`)
+    const titleElement = document.createElement("div");
+    titleElement.textContent = `${element.getTitle()}`;
+    titleElement.style.cssText = "font-size: 20px;  font-weight:bold;";
+    const iconContainer = document.createElement("div");
+    iconContainer.classList.add("priority-icon-container");
+    const actualIconPriority = document.createElement("span");
+    actualIconPriority.classList.add("material-symbols-outlined");
+    actualIconPriority.textContent = "circle";
+    if (element.getPriority() == "low"){
+      console.log("low")
+      actualIconPriority.style.cssText = "font-size: 20px; color:gray ;"
+    }else if(element.getPriority() == "medium"){
+      actualIconPriority.style.cssText = "font-size: 20px; color:yellow;"
+      console.log("medium");
+    }else if (element.getPriority() == "high"){
+      actualIconPriority.style.cssText = "font-size: 20px; color:red;";
+      console.log("high");
+    }
+    const dateElement = document.createElement("div");
+    dateElement.textContent = `${element.getDueDate()}`;
+    dateElement.style.cssText = "font-weight: 15px; font-weight: bold; justify-self: end; color: #374151;  font-style:italic;"
     listOfTodo.appendChild(todoCard);
+    todoCard.appendChild(titleElement);
+    todoCard.appendChild(iconContainer);
+    iconContainer.appendChild(actualIconPriority);
+    todoCard.appendChild(dateElement);
     console.log(element.printObj());
 })
 }
